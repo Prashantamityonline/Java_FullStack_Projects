1dc80f439d08f9e756a1ced3bf8c2380ed815375
Prasant Singh <prasant2@amityonline.com>
2025-04-28 16:33:40 +0530

    feat: Implement user update functionality with file upload support

--- DUCART/Ducart-Backend/src/main/java/com/prashant/api/ecom/ducart/controllers/UserController.java
+++ DUCART/Ducart-Backend/src/main/java/com/prashant/api/ecom/ducart/controllers/UserController.java
@@ -1,17 +1,24 @@
 package com.prashant.api.ecom.ducart.controllers;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prashant.api.ecom.ducart.entities.User;
 import com.prashant.api.ecom.ducart.modal.UserDTO;
 import com.prashant.api.ecom.ducart.services.UserService;
@@ -40,8 +47,36 @@ public class UserController {
 
   // get user by id
   @GetMapping("/{userid}")
-  public ResponseEntity<User> getUserById(@PathVariable Long userid) {
+  public ResponseEntity<User> getUserByID(@PathVariable Long userid) {
     User user = userService.getUserById(userid);
-    return ResponseEntity.status(HttpStatus.OK).body(user);
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.OK).body(user);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
   }
+
+  // update user by id
+  @PutMapping("/{userid}")
+  public ResponseEntity<Map<String, Object>> updateUserByID(@PathVariable Long userid,
+      @RequestPart("data") String jsonData,
+      @RequestPart("pic") MultipartFile file) {
+    try {
+      // Parse the JSON data to a UserDTO object
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.readValue(jsonData, UserDTO.class);
+
+      User updatedUser = userService.getUserById(userid);
+      Map<String, Object> response = new HashMap<>();
+      response.put("message", "User updated successfully");
+      response.put("data", updatedUser);
+      return ResponseEntity.status(HttpStatus.OK).body(response);
+    } catch (IOException e) {
+      Map<String, Object> error = new HashMap<>();
+      error.put("error", "Error updating user");
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+  }
+
 }
--- DUCART/Ducart-Backend/src/main/java/com/prashant/api/ecom/ducart/services/UserService.java
+++ DUCART/Ducart-Backend/src/main/java/com/prashant/api/ecom/ducart/services/UserService.java
@@ -1,19 +1,28 @@
 package com.prashant.api.ecom.ducart.services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.prashant.api.ecom.ducart.entities.User;
 import com.prashant.api.ecom.ducart.modal.UserDTO;
 import com.prashant.api.ecom.ducart.repositories.UserRepo;
+import com.prashant.api.ecom.ducart.utils.FileUploadUtil;
 
 @Service
 public class UserService {
   @Autowired
   private UserRepo userRepo;
+
+  // File upload directory
+  String uploadDir = FileUploadUtil.getUploadDirFor("users");
 
   // signup method
   public User signup(UserDTO userDTO) {
@@ -21,6 +30,43 @@ public class UserService {
     BeanUtils.copyProperties(userDTO, savedUser);
 
     return userRepo.save(savedUser);
+  }
+
+  // update user method
+  public User updateUser(Long userid, UserDTO userDTO, MultipartFile file) throws IOException {
+    User existUser = userRepo.findById(userid)
+        .orElseThrow(() -> new RuntimeException("User not found by id: " + userid));
+    // update user details
+    userDTO.setName(userDTO.getName());
+    userDTO.setPhone(userDTO.getPhone());
+    userDTO.setAddress(userDTO.getAddress());
+    userDTO.setCity(userDTO.getCity());
+    userDTO.setState(userDTO.getState());
+    userDTO.setPin(userDTO.getPin());
+    userDTO.setPic(userDTO.getPic());
+
+    // update user image
+    if (file != null && !file.isEmpty()) {
+      String relativePath = saveFile(file);
+      userDTO.setPic(relativePath);
+      if (existUser != null) {
+        BeanUtils.copyProperties(userDTO, existUser);
+
+        return userRepo.save(existUser);
+      } else {
+        throw new RuntimeException("User not found by id: " + userid);
+      }
+
+    }
+    return existUser;
+  }
+
+  // save file method
+  public String saveFile(MultipartFile file) throws IOException {
+    String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+    Path filePath = Paths.get(uploadDir, fileName);
+    Files.write(filePath, file.getBytes());
+    return "/uploads/users/" + fileName;
   }
 
   // // login method
