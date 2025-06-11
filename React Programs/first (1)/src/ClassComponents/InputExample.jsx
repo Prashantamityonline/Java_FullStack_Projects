31ca44c16f38bb3dc78d3a606afa99ffdf43da30
Prasant Singh <prasant2@amityonline.com>
2025-06-11 23:46:41 +0530

    commit

--- React Programs/first (1)/src/ClassComponents/InputExample.jsx
+++ React Programs/first (1)/src/ClassComponents/InputExample.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class InputExample extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            email: "",
-            phone: "",
-            designation: "",
-            salary: "",
-            city: "",
-            state: ""
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      designation: "",
+      salary: "",
+      city: "",
+      state: "",
+    };
+  }
 
-    getInputData(e) {
-        let { name, value } = e.target
-        this.setState(() => {
-            return {
-                ...this.state,
-                [name]: value
-            }
-        })
-    }
-    postSubmit(e) {
-        e.preventDefault()
-        alert(`
+  getInputData(e) {
+    let { name, value } = e.target;
+    this.setState(() => {
+      return {
+        ...this.state,
+        [name]: value,
+      };
+    });
+  }
+  postSubmit(e) {
+    e.preventDefault();
+    alert(`
                Name             :   ${this.state.name}
                Email            :   ${this.state.email}
                Phone            :   ${this.state.phone}
@@ -33,39 +33,79 @@ export default class InputExample extends Component {
                Salary           :   ${this.state.salary}
                City             :   ${this.state.city}
                State            :   ${this.state.state}
-            `)
-    }
-    render() {
-        return (
-            <>
-                <div className="main">
-                    <div className="center">
-                        <h1>Class Component Input Example</h1>
-                        <h2>Name  : {this.state.name}</h2>
-                        <h2>Email  : {this.state.email}</h2>
-                        <h2>Phone  : {this.state.phone}</h2>
-                        <h2>Designation  : {this.state.designation}</h2>
-                        <h2>Salary  : {this.state.salary}</h2>
-                        <h2>City  : {this.state.city}</h2>
-                        <h2>State  : {this.state.state}</h2>
-                        <form onSubmit={(e) => this.postSubmit(e)}>
-                            <input type="text" name="name" required onChange={(e) => this.getInputData(e)} placeholder='Full Name' />
-                            <input type="email" name="email" required onChange={(e) => this.getInputData(e)} placeholder='Email Address' />
-                            <input type="text" name="phone" required onChange={(e) => this.getInputData(e)} placeholder='Phone Number' />
-                            <input type="text" name="designation" required onChange={(e) => this.getInputData(e)} placeholder='Designation' />
-                            <input type="number" name="salary" required onChange={(e) => this.getInputData(e)} placeholder='Salary' />
-                            <input type="text" name="city" required onChange={(e) => this.getInputData(e)} placeholder='City Name' />
-                            <input type="text" name="state" required onChange={(e) => this.getInputData(e)} placeholder='State Name' />
-                            <button type='submit'>Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </>
-        )
-    }
+            `);
+  }
+  render() {
+    return (
+      <>
+        <div className="main">
+          <div className="center">
+            <h1>Class Component Input Example</h1>
+            <h2>Name : {this.state.name}</h2>
+            <h2>Email : {this.state.email}</h2>
+            <h2>Phone : {this.state.phone}</h2>
+            <h2>Designation : {this.state.designation}</h2>
+            <h2>Salary : {this.state.salary}</h2>
+            <h2>City : {this.state.city}</h2>
+            <h2>State : {this.state.state}</h2>
+            <form onSubmit={(e) => this.postSubmit(e)}>
+              <input
+                type="text"
+                name="name"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="Full Name"
+              />
+              <input
+                type="email"
+                name="email"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="Email Address"
+              />
+              <input
+                type="text"
+                name="phone"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="Phone Number"
+              />
+              <input
+                type="text"
+                name="designation"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="Designation"
+              />
+              <input
+                type="number"
+                name="salary"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="Salary"
+              />
+              <input
+                type="text"
+                name="city"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="City Name"
+              />
+              <input
+                type="text"
+                name="state"
+                required
+                onChange={(e) => this.getInputData(e)}
+                placeholder="State Name"
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-
 
 // import React, { Component } from 'react'
 
@@ -144,8 +184,6 @@ export default class InputExample extends Component {
 //         )
 //     }
 // }
-
-
 
 // import React, { Component } from 'react'
 
